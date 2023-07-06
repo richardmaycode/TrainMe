15dbdbdc45c615ac737f6657493a54316bb71379
Richard Wise <richard.wise@hey.com>
2023-07-05 21:12:43 -0400

    Initial Commit

---
+++ TrainMe/TrainMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainMeApp.swift
+//  TrainMe
+//
+//  Created by Richard Wise on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
